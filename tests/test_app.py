c89c8b4301eedb7e30607cbff573760952772c39
Dhanunjaya-Elluri <dhanunjayet@gmail.com>
2024-08-24 19:27:10 +0200

    Added Multiply function

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,11 @@
 import pytest
-from app.app import add, subtract
+from app.app import add, subtract, multiply
 
 def test_add():
     assert add(1, 2) == 3
 
 def test_subtract():
     assert subtract(2, 1) == 1
+
+def test_multiply():
+    assert multiply(2, 3) == 6
